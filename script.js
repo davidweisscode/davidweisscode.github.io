daa5d9ed596942db4550a297ae29a1cd2da7a56d
david <davidweisspost@gmail.com>
2019-04-15 12:18:51 +0200

    Draft randomized slide order

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ class Slideshow {
     initSlides() {
         this.container = $('[data-slideshow]'); // Select by attribute --> img-container
         this.slides = this.container.find('img'); // Get all elements
-        this.slides.each((idx, slide) => $(slide).attr('data-slide', idx));
+        shuffleArray(this.slides); // Randomize slide order
+        this.slides.each((idx, slide) => {
+            $(slide).attr('slide-id', $(slide).attr('id'));
+            $(slide).attr('slide-description', slideDescription[$(slide).attr('id')]);
+        });
+        
     }
 
     initSlideshow() {
@@ -15,11 +20,11 @@ class Slideshow {
         this.currentIndex = 0;
         this.setNextSlide();
         this.slides.each((idx, slide) => {
-          $('<img>').on('load', $.proxy(this.loadImage, this)).attr('src', $(slide).attr('src')); // ?
+          $('<img>').on('load', $.proxy(this.finishLoadingImage, this)).attr('src', $(slide).attr('src'));
         });
     }
 
-    loadImage() {
+    finishLoadingImage() {
         this.imagesLoaded++;
         if(this.imagesLoaded >= this.slides.length) {
             this.playSlideshow();
@@ -27,7 +32,7 @@ class Slideshow {
     }
 
     setNextSlide() {
-        this.nextSlide = this.container.find(`[data-slide="${this.currentIndex}"]`).first();
+        this.nextSlide = this.container.find(`[slide-id="${this.currentIndex}"]`).first();
         this.nextSlide.addClass('next');
     }
 
@@ -52,7 +57,24 @@ class Slideshow {
 
         this.setNextSlide();
 
+        $('.slide-text').text(this.nextSlide.attr('slide-description'));
+
         this.prevSlide.addClass('fade-out');
+    }
+}
+
+const slideDescription = [
+    "First descr",
+    "Second descr",
+    "Third descr",
+];
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
 
