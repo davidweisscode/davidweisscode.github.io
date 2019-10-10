031e5e83e3582f27015bc91ca2a23138e2c0f374
david <davidweisspost@gmail.com>
2019-10-10 13:03:40 +0200

    Use relative links

--- slides.js
+++ slides.js
@@ -4,7 +4,7 @@ export let slides = [
         "title": "MyTitle1",
         "header": "MyHeader1 is a short and interesting header.",
         "description": "My description 1 is a longer explanation of what this project was all about. It contains multiple sentences and links to other resources and websites. There should be linebreaks and text <b>formatting</b>.<br>This is a line break. This text will be interpreted as html. This could possibly be an <a href='https://www.google.com'>external link</a> to some other website.",
-        "link": "http://localhost:8080/projects.html#1",
+        "link": "projects.html#1",
         "image": "svg/drawing-1.svg",
     },
     {
@@ -12,7 +12,7 @@ export let slides = [
         "title": "MyTitle2",
         "header": "MyHeader2 is a short and interesting header.",
         "description": "My description 2 is a longer explanation of what this project was all about. This text will be interpreted as html. This could possibly be an <a href='https://www.google.com'>external link</a> to some other website. There should be linebreaks and text <b>formatting</b>.<br>This is a line break. It contains multiple sentences and links to other resources and websites. ",
-        "link": "http://localhost:8080/projects.html#2",
+        "link": "projects.html#2",
         "image": "svg/drawing-2.svg",
     },
     {
@@ -20,7 +20,7 @@ export let slides = [
         "title": "MyTitle3",
         "header": "MyHeader3 is a short and interesting header.",
         "description": "My description 3 is a longer explanation of what this project was all about. This could possibly be an <a href='https://www.google.com'>external link</a> to some other website. There should be linebreaks and text <b>formatting</b>.<br>This is a line break. This text will be interpreted as html. It contains multiple sentences and links to other resources and websites. ",
-        "link": "http://localhost:8080/projects.html#3",
+        "link": "projects.html#3",
         "image": "svg/drawing-3.svg",
     },
 ]
